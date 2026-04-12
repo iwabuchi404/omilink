3fed7e197746c925d4c480cdeeee93fca7ff3f7d
iwabuchi <iwabuti404@gmail.com>
2026-04-13 02:13:27 +0900

    開発環境でのエラー表示設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import checker from 'vite-plugin-checker'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      vueTsc: true,
+    }),
+  ],
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:8090',
